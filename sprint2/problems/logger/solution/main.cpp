13b671355e0940387ae6c6036b31b276e48e7d5f
buschrutt@gmail.com <buschrutt@gmail.com>
2023-06-11 19:35:12 -0500

    logger

--- sprint2/problems/logger/solution/main.cpp
+++ sprint2/problems/logger/solution/main.cpp
@@ -8,23 +8,23 @@ using namespace std::literals;
 int main() {
     // Будем устанавливать моменты времени в секундах от начала эпохи.
     // Конкретные значения не так важны, главное, что часы идут монотонно.
-    Logger::GetInstance().SetTimestamp(std::chrono::system_clock::time_point{1000000s});
+    Logger::GetInstance().SetTimestamp(std::chrono::system_clock::time_point{1'000'000s});
 
     // Логируем значения разных типов.
     LOG("Hello "sv, "world "s, 123);
 
     // Проверяем, что логер можно вызвать с очень большим количеством параметров.
-    LOG(1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 
+    LOG(1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0,
         1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
-    
-    Logger::GetInstance().SetTimestamp(std::chrono::system_clock::time_point{10000000s});
+
+    Logger::GetInstance().SetTimestamp(std::chrono::system_clock::time_point{10'000'000s});
     LOG("Brilliant logger.", " ", "I Love it");
 
     // Выполним ещё 100000 логирований.
-    static const int attempts = 100000;
+    static const int attempts = 50;
     for(int i = 0; i < attempts; ++i) {
-        std::chrono::system_clock::time_point ts(std::chrono::seconds(10000000 + i * 100));
+        std::chrono::system_clock::time_point ts(std::chrono::seconds(10'000'000 + i * 100));
         Logger::GetInstance().SetTimestamp(ts);
 
         LOG("Logging attempt ", i, ". ", "I Love it");
--- sprint2/problems/logger/solution/my_logger.h
+++ sprint2/problems/logger/solution/my_logger.h
@@ -9,47 +9,75 @@
 #include <optional>
 #include <mutex>
 #include <thread>
+#include <iostream>
 
 using namespace std::literals;
 
 #define LOG(...) Logger::GetInstance().Log(__VA_ARGS__)
 
 class Logger {
-    auto GetTime() const {
-        if (manual_ts_) {
+    [[nodiscard]] auto GetTime() const {
+        if (manual_ts_)
             return *manual_ts_;
-        }
-
         return std::chrono::system_clock::now();
     }
 
-    auto GetTimeStamp() const {
-        const auto now = GetTime();
-        const auto t_c = std::chrono::system_clock::to_time_t(now);
-        return std::put_time(std::localtime(&t_c), "%F %T");
+    std::string GetTimeStamp() {
+        const auto t_c = std::chrono::system_clock::to_time_t(GetTime());
+        std::tm local_time{};
+        localtime_s(&local_time, &t_c);
+        std::stringstream ss;
+        ss << std::put_time(&local_time, "%F %T");
+        return ss.str();
     }
 
-    // Для имени файла возьмите дату с форматом "%Y_%m_%d"
-    std::string GetFileTimeStamp() const;
+    [[nodiscard]] std::string GetFileTimeStamp() const {
+        const auto t_c = std::chrono::system_clock::to_time_t(GetTime());
+        std::tm local_time{};
+        localtime_s(&local_time, &t_c);
+        std::stringstream ss;
+        ss << std::put_time(&local_time, "%Y_%m_%d");
+        return ss.str();
+    }
 
     Logger() = default;
-    Logger(const Logger&) = delete;
 
 public:
+    Logger(const Logger&) = delete;
+
     static Logger& GetInstance() {
         static Logger obj;
         return obj;
     }
 
-    // Выведите в поток все аргументы.
-    template<class... Ts>
-    void Log(const Ts&... args);
+    //// <<< PRINT ARGS >>>
+    template <class T>
+    void PrintArgsToFile(std::ostream& os, const T& arg) {
+        os << arg;
+    }
+    //// <<< >>>
+    template <class T, class... Ts>
+    void PrintArgsToFile(std::ostream& os, const T& arg, const Ts&... args) {
+        os << arg;
+        PrintArgsToFile(os, args...);
+    }
 
-    // Установите manual_ts_. Учтите, что эта операция может выполняться
-    // параллельно с выводом в поток, вам нужно предусмотреть 
-    // синхронизацию.
-    void SetTimestamp(std::chrono::system_clock::time_point ts);
+    template<class... Ts>
+    void Log(const Ts&... args) {
+        std::lock_guard<std::mutex> lock(m_);
+        std::ofstream log_file("/var/log/sample_log_" + GetFileTimeStamp(), std::ios_base::app); // "/var/log/sample_log_"
+        log_file << GetTimeStamp() << ": ";
+        PrintArgsToFile(log_file, args...);
+        log_file << std::endl;
+    }
+
+    void SetTimestamp(std::chrono::system_clock::time_point ts) {
+        std::lock_guard<std::mutex> lock(m_);
+        manual_ts_ = ts;
+    }
 
 private:
     std::optional<std::chrono::system_clock::time_point> manual_ts_;
+    std::ofstream log_file_{"sample.log"s};
+    std::mutex m_;
 };
